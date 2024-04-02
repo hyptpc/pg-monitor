25948327dbfecbfd49c0fedc42ca8ad9bb9d1a1c
Shuhei Hayakawa <shuhei.hayakawa.c8@tohoku.ac.jp>
2024-04-03 01:24:11 +0900

    Modify recorder.py

--- recorder.py
+++ recorder.py
@@ -6,6 +6,7 @@ __author__ = 'Shuhei Hayakawa'
 Recorder script for slow monitor system using PostgreSQL database
 '''
 
+import datetime
 import gzip
 import logging
 import logging.config
@@ -28,6 +29,14 @@ rotators = dict()
 tables = None
 
 #______________________________________________________________________________
+def execute_sql(sql):
+  with psycopg.connect(pgpass.pgpass) as connection:
+    with connection.cursor() as cursor:
+      logger.debug(sql)
+      cursor.execute(sql)
+      return cursor.fetchall()
+
+#______________________________________________________________________________
 def __make_rotator(table_name):
   global rotators
   if table_name not in rotators:
@@ -41,41 +50,61 @@ def __make_rotator(table_name):
     rotators[table_name] = (file_name, rotator, handler)
 
 #______________________________________________________________________________
-def __record_single(table_name):
-  logger.info(f'start {table_name}')
-  rotators[table_name][2].doRollover()
+def __record_single(table_name, sql, file_name, doRollover=True):
+  logger.info(f'start {table_name} ({file_name})')
+  if doRollover:
+    rotators[table_name][2].doRollover()
   with psycopg.connect(pgpass.pgpass) as connection:
     with connection.cursor() as cursor:
-      sql = f"COPY {table_name} TO stdout DELIMITER ',' CSV HEADER"
-      # sql = f"COPY (SELECT * FROM {table_name} ORDER BY timestamp DESC LIMIT 100) TO stdout DELIMITER ',' CSV HEADER"
       logger.debug(sql)
       with cursor.copy(sql) as copy:
-        with gzip.open(rotators[table_name][0], 'wb') as f:
+        with gzip.open(file_name, 'wb') as f:
           for data in copy:
             f.write(data)
-  logger.info(f'recorded {table_name}')
+  logger.info(f'recorded {table_name} ({file_name})')
 
 #______________________________________________________________________________
-def record():
+def __record_all():
   global tables
   try:
-    log_conf = os.path.join(top_dir, 'logging_config.yml')
-    with open(log_conf, 'r') as f:
-      logging.config.dictConfig(yaml.safe_load(f))
-    with psycopg.connect(pgpass.pgpass) as connection:
-      with connection.cursor() as cursor:
-        sql = 'SELECT relname FROM pg_stat_user_tables'
-        logger.debug(sql)
-        cursor.execute(sql)
-        tables = cursor.fetchall()
+    tables = execute_sql('SELECT relname FROM pg_stat_user_tables')
     for t in tables:
       if len(t) != 1:
         continue
       table_name = t[0]
       __make_rotator(table_name)
-      t = threading.Thread(target=__record_single, args=(table_name,))
+      ''' all '''
+      sql = f"COPY {table_name} TO stdout DELIMITER ',' CSV HEADER"
+      file_name = rotators[table_name][0]
+      t = threading.Thread(target=__record_single,
+                           args=(table_name, sql, file_name))
       t.daemon = True
       t.start()
+      ''' every day '''
+      start = datetime.date(2024, 4, 1)
+      end = datetime.date.today()
+      for i in range((end - start).days):
+        date = start + datetime.timedelta(i)
+        daily_name = os.path.join(
+          top_dir, 'log', 'daily',
+          f'pg-{pgpass.dbname}-{table_name}-{str(date)}.csv.gz')
+        if os.path.isfile(daily_name):
+          continue
+        sql = (f"SELECT COUNT(1) FROM {table_name} WHERE timestamp " +
+               f"BETWEEN '{date}' AND '{date + datetime.timedelta(1)}'")
+        count = execute_sql(sql)[0][0]
+        if count == 0:
+          continue
+        sql = (f"COPY (SELECT * FROM {table_name} WHERE timestamp " +
+               f"BETWEEN '{date}' AND '{date + datetime.timedelta(1)}') " +
+               f"TO stdout DELIMITER ',' CSV HEADER")
+        logger.debug(date)
+        logger.debug(sql)
+        logger.debug(daily_name)
+        t = threading.Thread(target=__record_single,
+                             args=(table_name, sql, daily_name, False))
+        t.daemon = True
+        t.start()
   except KeyboardInterrupt:
     return
   thread_list = threading.enumerate()
@@ -84,12 +113,19 @@ def record():
     t.join()
 
 #______________________________________________________________________________
+def __record_one_day():
+  pass
+
+#______________________________________________________________________________
 def run(interval=60*60*24):
+  log_conf = os.path.join(top_dir, 'logging_config.yml')
+  with open(log_conf, 'r') as f:
+    logging.config.dictConfig(yaml.safe_load(f))
   base_time = time.time()
   next_time = 0
   while True:
     try:
-      record()
+      __record_all()
       logger.info('waiting next record ...')
       next_time = ((base_time - time.time()) % interval) or interval
       time.sleep(next_time)
@@ -100,4 +136,4 @@ def run(interval=60*60*24):
 #______________________________________________________________________________
 if __name__ == '__main__':
   run()
-  
+
