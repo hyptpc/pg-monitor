1f987262b48bdd7bf227cf92079b6952d3f20cee
Shuhei Hayakawa <shuhei.hayakawa.c8@tohoku.ac.jp>
2025-02-13 23:55:48 +0900

    Rename grafana dashboard json.

--- grafana-dashboard/get-dashboard.sh
+++ grafana-dashboard/get-dashboard.sh
@@ -8,9 +8,12 @@ dashboard_uid=fdh0b69n4k0lca
 dashboard_list=$(curl -s -H "Authorization: Bearer $api_key" "http://$grafana_host:$port/api/search?type=dash-db")
 
 for uid in $(echo "$dashboard_list" | jq -r '.[].uid'); do
-curl -H \
-     "Authorization: Bearer $api_key" \
-     -H "Content-Type: application/json" \
-     http://$grafana_host:$port/api/dashboards/uid/$uid \
-     -o dashboard_$uid.json
+    dashboard_json=$(curl -s -H "Authorization: Bearer $api_key" \
+                          -H "Content-Type: application/json" \
+                          "http://$grafana_host:$port/api/dashboards/uid/$uid")
+    dashboard_title=$(echo "$dashboard_json" | jq -r '.dashboard.title')
+    safe_title=$(echo "$dashboard_title" | tr -cd '[:alnum:]-_')
+    output_file="${safe_title}.json"
+    echo "$dashboard_json" > "$output_file"
+    echo "Saved dashboard UID: $uid as $output_file"
 done
