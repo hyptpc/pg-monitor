e4483e447b507129815723c95e9bbd3c3900c11d
Shuhei Hayakawa <shuhei.hayakawa.c8@tohoku.ac.jp>
2025-04-19 08:08:48 +0900

    Add ess.

--- module/ess.py
+++ module/ess.py
@@ -1,104 +1,59 @@
-#!/usr/bin/env python3
-
-import datetime
-import logging
-import pandas as pd
-import psycopg
-import pytz
-import subprocess
-import threading
 import time
+from datetime import datetime, timezone
+from epics import caget
+import psycopg2
+import logging
 
-logger = logging.getLogger('__main__').getChild(__name__)
+logging.basicConfig(level=logging.INFO,
+                    format='%(levelname)s %(message)s')
+logger = logging.getLogger()
 
-class ESS:
-  def __init__(self, interval=10):
-    self.will_stop = False
-    self.url = 'http://www-cont.j-parc.jp/HD/separator'
-    self.interval = interval
-    self.wait = True
+DB_CONFIG = {
+  'host': 'localhost',
+  'port': 5432,
+  'dbname': 'e72',
+  'user': 'oper',
+  'password': 'himitsu'
+}
 
-  def __parse(self):
-    return pd.read_html(self.url)
+PV_MAP = {
+  'HDESS:K18_ESS1:NEG_VSET': 'neg_vset1',
+  'HDESS:K18_ESS1:POS_VSET': 'pos_vset1',
+  'HDESS:K18_ESS1:NEG_VMON': 'neg_vmon1',
+  'HDESS:K18_ESS1:POS_VMON': 'pos_vmon1',
+  'HDESS:K18_ESS1:NEG_IMON': 'neg_imon1',
+  'HDESS:K18_ESS1:POS_IMON': 'pos_imon1',
+  'HDESS:K18_ESS1:CCG_PMON': 'ccg_pmon1',
+  'HDESS:K18_ESS2:NEG_VSET': 'neg_vset2',
+  'HDESS:K18_ESS2:POS_VSET': 'pos_vset2',
+  'HDESS:K18_ESS2:NEG_VMON': 'neg_vmon2',
+  'HDESS:K18_ESS2:POS_VMON': 'pos_vmon2',
+  'HDESS:K18_ESS2:NEG_IMON': 'neg_imon2',
+  'HDESS:K18_ESS2:POS_IMON': 'pos_imon2',
+  'HDESS:K18_ESS2:CCG_PMON': 'ccg_pmon2'
+}
 
-  def run(self):
-    base_time = time.time()
-    next_time = 0
-    while not self.will_stop:
-      try:
-        t = threading.Thread(target=self.__updater)
-        t.daemon = True
-        t.start()
-        if self.wait:
-          t.join()
-        next_time = ((base_time - time.time()) % self.interval) or self.interval
-        time.sleep(next_time)
-      except KeyboardInterrupt:
-        break
-
-  def __updater(self):
-    logger.debug(datetime.datetime.now())
-    connection = psycopg.connect('host=localhost dbname=e73 user=postgres password=pg')
-    cursor = connection.cursor()
-    alert = False
+#_________________________________
+def main():
+  while True:
     try:
-      insert_list = []
-      now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
-      data = self.__parse()
-      if len(data) < 3:
-        return
-      for i in range(2):
-        logger.debug(data[i])
-        name = f'ESS{i+1}'
-        for row in data[i].itertuples():
-          electrode = row[1]
-          vset = row[2]
-          vmon = row[3]
-          imon = row[4]
-          if imon > 50:
-            alert = True
-          insert_list.append((name, now, electrode, vset, vmon, imon, None))
-        #   insert_list.append(tap)
-      logger.debug(data[2])
-      for row in data[2].itertuples():
-        name = row[1]
-        vacuum = row[2].split()[0]
-        insert_list.append((name, now, None, None, None, None, vacuum))
-      sql = ('insert into ess ' +
-             '(name, timestamp, electrode, vset, vmon, imon, vacuum) '
-             +'values(%s,%s,%s,%s,%s,%s,%s)')
-      cursor.executemany(sql, insert_list)
-      if alert:
-        logger.warning('detect alert')
-        subprocess.run(['aplay', '/home/oper/postgres/e73/alert_sound.wav'])
-    except psycopg.Error as e:
-      connection.rollback()
-      logger.error(e.diag.message_primary)
-      return
-    except KeyboardInterrupt:
-      return
-    connection.commit()
-    cursor.close()
-    connection.close()
-
-  def start(self):
-    logger.debug('start')
-    t = threading.Thread(target=self.run)
-    t.daemon = True
-    t.start()
-
-  def stop(self):
-    logger.debug('stop')
-    self.will_stop = True
-
-e = ESS()
-
-def start():
-  e.start()
-
-def stop():
-  e.stop()
+      timestamp = datetime.now(timezone.utc)
+      values = {col: caget(pv) for pv, col in PV_MAP.items()}
+      with psycopg2.connect(**DB_CONFIG) as conn:
+        with conn.cursor() as cur:
+          sql = """
+            INSERT INTO ess (timestamp, {columns})
+            VALUES (%s, {placeholders})
+            """.format(
+              columns=", ".join(values.keys()),
+              placeholders=", ".join(["%s"] * len(values))
+            )
+          cur.execute(sql, [timestamp] + list(values.values()))
+          conn.commit()
+      logger.debug("Logged values at %s", timestamp.isoformat())
+    except Exception as e:
+      logger.error("Error during logging: %s", e)
+    time.sleep(10)
 
 if __name__ == '__main__':
-  e = ESS()
-  e.run()
+  main()
--- sql/create-table-ess.sql
+++ sql/create-table-ess.sql
@@ -1,9 +1,28 @@
-CREATE TABLE ess
-(name  TEXT,
- timestamp   TIMESTAMP WITH TIME ZONE,
- electrode   TEXT,
- vset        REAL,
- vmon        REAL,
- imon        REAL,
- vacuum      REAL
+CREATE TABLE ess (
+  timestamp  TIMESTAMPTZ PRIMARY KEY,
+  neg_vset1  REAL,
+  neg_vmon1  REAL,
+  neg_imon1  REAL,
+  pos_vset1  REAL,
+  pos_vmon1  REAL,
+  pos_imon1  REAL,
+  ccg_pmon1  REAL,
+  neg_vset2  REAL,
+  neg_vmon2  REAL,
+  neg_imon2  REAL,
+  pos_vset2  REAL,
+  pos_vmon2  REAL,
+  pos_imon2  REAL,
+  ccg_pmon2  REAL
 );
+
+-- -- for e73
+-- CREATE TABLE ess
+-- (name  TEXT,
+--  timestamp   TIMESTAMP WITH TIME ZONE,
+--  electrode   TEXT,
+--  vset        REAL,
+--  vmon        REAL,
+--  imon        REAL,
+--  vacuum      REAL
+-- );
